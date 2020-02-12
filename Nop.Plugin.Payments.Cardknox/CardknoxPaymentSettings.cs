483c22d8e0f467b7f0dfa2f2114aea6b8965bad5
Vinicius Mabellini <vpmabellini@gmail.com>
2020-02-12 11:47:43 -0300

    First commit

---
+++ Nop.Plugin.Payments.Cardknox/CardknoxPaymentSettings.cs
@@ -0,0 +1,48 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.Cardknox
+{
+	public class CardknoxPaymentSettings : ISettings
+	{
+		public bool UseSandbox { get; set; }
+		public TransactMode TransactMode { get; set; }
+		public string TransactionKey { get; set; }
+		public string SoftwareName { get; set; }
+		public string SoftwareVersion { get; set; }
+
+		/// <summary>
+		/// Overrides the default API version sent by the Cardknox SDK
+		/// </summary>
+		public bool OverrideApiVersion { get; set; }
+
+		/// <summary>
+		/// Api Version to send to the Cardknox SDK
+		/// </summary>
+		public string ApiVersion { get; set; }
+
+		/// <summary>
+		/// Doesn't send the address details to Cardknox
+		/// </summary>
+		public bool HideAddressDetails { get; set; }
+
+		/// <summary>
+		/// Uses the shipping address as the billing address
+		/// </summary>
+		public bool UseShippingAddressAsBilling { get; set; }
+
+		/// <summary>
+		/// Sends an receipt to the customer e-mail
+		/// </summary>
+		public bool SendReceiptToCustomerEmail { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether to "additional fee" is specified as percentage. true - percentage, false - fixed value.
+		/// </summary>
+		public bool AdditionalFeePercentage { get; set; }
+
+		/// <summary>
+		/// Additional fee
+		/// </summary>
+		public decimal AdditionalFee { get; set; }
+	}
+}
---
+++ Nop.Plugin.Payments.Cardknox/Components/CardknoxViewComponent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Plugin.Payments.Cardknox.Models;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Payments.Cardknox.Components
+{
+	[ViewComponent(Name = "Cardknox")]
+	public class CardknoxViewComponent : NopViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var model = new PaymentInfoModel();
+
+			//years
+			for (var i = 0; i < 15; i++)
+			{
+				var year = Convert.ToString(DateTime.Now.Year + i);
+				model.ExpireYears.Add(new SelectListItem
+				{
+					Text = year,
+					Value = year,
+				});
+			}
+
+			//months
+			for (var i = 1; i <= 12; i++)
+			{
+				var text = i < 10 ? "0" + i : i.ToString();
+				model.ExpireMonths.Add(new SelectListItem
+				{
+					Text = text,
+					Value = i.ToString(),
+				});
+			}
+
+			//set postback values (we cannot access "Form" with "GET" requests)
+			if (Request.Method == WebRequestMethods.Http.Get)
+				return View("~/Plugins/Payments.Cardknox/Views/PaymentInfo.cshtml", model);
+
+			var form = Request.Form;
+			model.CardholderName = form["CardholderName"];
+			model.CardNumber = form["CardNumber"];
+			model.CardCode = form["CardCode"];
+			var selectedMonth = model.ExpireMonths.FirstOrDefault(x =>
+				x.Value.Equals(form["ExpireMonth"], StringComparison.InvariantCultureIgnoreCase));
+
+			if (selectedMonth != null)
+				selectedMonth.Selected = true;
+
+			var selectedYear = model.ExpireYears.FirstOrDefault(x =>
+				x.Value.Equals(form["ExpireYear"], StringComparison.InvariantCultureIgnoreCase));
+
+			if (selectedYear != null)
+				selectedYear.Selected = true;
+
+			return View("~/Plugins/Payments.Cardknox/Views/PaymentInfo.cshtml", model);
+		}
+	}
+}
